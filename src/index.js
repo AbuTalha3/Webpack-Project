0b02291acb28dab5e1e2dbca45b45a59bb72a6f3
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-13 03:17:24 +0500

    Update the required files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import _ from "lodash";
-
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
+import './style.css';
